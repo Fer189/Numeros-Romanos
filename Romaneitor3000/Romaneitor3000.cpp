16443b5fdeec3f297560cb28b0941b70e168746b
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-17 17:07:35 -0600

    Añadido caracter L

--- Romaneitor3000/Romaneitor3000.cpp
+++ Romaneitor3000/Romaneitor3000.cpp
@@ -6,8 +6,15 @@ void comprobarNumero(char numero[]) {
     int I = 0;
     int V = 0;
     int X = 0;
-    for (int i = 0; i <= sizeof(numero) - 1; i++) {
-        if (numero[i] != 'I' && numero[i] != 'V') {
+    int L = 0;
+    int cantidad = 0;
+    for (int i = 0; i <= 20; i++) {
+        if (numero[i] != '\0') {
+            cantidad++;
+        }
+    }
+    for (int i = 0; i <= cantidad - 1; i++) {
+        if (numero[i] != 'I' && numero[i] != 'V' && numero[i] != 'X' && numero[i] != 'L') {
             cout << "Digito " << i + 1 << " Es incorrecto\n";
         }
         switch (numero[i]) {
@@ -20,6 +27,9 @@ void comprobarNumero(char numero[]) {
         case 'X':
             X++;
             break;
+        case 'L':
+            L++;
+            break;
         }
     }
     if (I > 3 || V > 3 || X > 3) {
@@ -27,21 +37,11 @@ void comprobarNumero(char numero[]) {
     }
 }
 
-int contarCaracteres(char numero[]) {
-    int cantidad = 0;
-    for (int i = 0; i <= 20; i++) {
-        if (numero[i] != '\0') {
-            cantidad++;
-        }
-    }
-    return cantidad;
-}
-
-void main() {
+int main() {
     cout << "Hello World!\n";
     cout << "Ingrese un numero romano" << endl;
     cin >> numeroRomano;
     cout << numeroRomano << endl;
-    //comprobarNumero(numeroRomano);
-    cout << contarCaracteres(numeroRomano) << endl;
+    comprobarNumero(numeroRomano);
+    return 0;
 }
