dfb36ffd662f6fb87b916dc4c6cc056f405f3b2b
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-17 16:55:03 -0600

    Comprobacion de numero romano

--- Romaneitor3000/Romaneitor3000.cpp
+++ Romaneitor3000/Romaneitor3000.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
+char numeroRomano[20];
 
-int main() {
+void comprobarNumero(char numero[]) {
+    int I = 0;
+    int V = 0;
+    int X = 0;
+    for (int i = 0; i <= sizeof(numero) - 1; i++) {
+        if (numero[i] != 'I' && numero[i] != 'V') {
+            cout << "Digito " << i + 1 << " Es incorrecto\n";
+        }
+        switch (numero[i]) {
+        case 'I':
+            I++;
+            break;
+        case 'V':
+            V++;
+            break;
+        case 'X':
+            X++;
+            break;
+        }
+    }
+    if (I > 3 || V > 3 || X > 3) {
+        cout << "Numero Incorrecto\n";
+    }
+}
+
+void main() {
     cout << "Hello World!\n";
-
+    cout << "Ingrese un numero romano" << endl;
+    cin >> numeroRomano;
+    cout << numeroRomano << endl;
+    comprobarNumero(numeroRomano);
 }
