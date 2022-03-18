70421860a0fcc3a84c636562ab9a1076bb7a370b
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-17 19:05:58 -0600

    Calculo programado

--- Romaneitor3000/Romaneitor3000.cpp
+++ Romaneitor3000/Romaneitor3000.cpp
@@ -12,9 +12,6 @@ bool numeroEsCorrecto(char numero[]) {
     int cantidad = 0;
     int errores = 0;
     for (int i = 0; numero[i] != '\0'; i++) {
-            cantidad++;
-    }
-    for (int i = 0; i <= cantidad - 1; i++) {
         if (numero[i] != 'I' && numero[i] != 'V' && numero[i] != 'X' && numero[i] != 'L' && numero[i] != 'C' && numero[i] != 'D' && numero[i] != 'M') {
             cout << "Digito " << i + 1 << " Es incorrecto\n";
             errores++;
@@ -33,19 +30,50 @@ bool numeroEsCorrecto(char numero[]) {
             L++;
             break;
         case 'C':
-            L++;
+            C++;
             break;
         case 'D':
-            L++;
+            D++;
             break;
         case 'M':
-            L++;
+            M++;
             break;
         }
-    }
-    if (I > 3 || V > 3 || X > 3 || C > 3 || D > 3 || M > 3) {
-        cout << "Numero Incorrecto\n";
-        errores++;
+        if ((numero[i] == 'I' && numero[i + 1] == 'L') || (numero[i] == 'I' && numero[i + 1] == 'C') || (numero[i] == 'I' && numero[i + 1] == 'D') || (numero[i] == 'I' && numero[i + 1] == 'M')) {
+            errores++;
+        }
+        if ((numero[i] == 'V' && numero[i + 1] == 'X') || (numero[i] == 'V' && numero[i + 1] == 'L') || (numero[i] == 'V' && numero[i + 1] == 'C') || (numero[i] == 'V' && numero[i + 1] == 'D') || (numero[i] == 'V' && numero[i + 1] == 'M')) {
+            errores++;
+        }
+        if ((numero[i] == 'X' && numero[i + 1] == 'D') || (numero[i] == 'X' && numero[i + 1] == 'M')) {
+            errores++;
+        }
+        if ((numero[i] == 'L' && numero[i + 1] == 'C') || (numero[i] == 'L' && numero[i + 1] == 'D') || (numero[i] == 'L' && numero[i + 1] == 'M')) {
+            errores++;
+        }
+        //No se permiten repeticiones de mas de tres letras iguales consecutivas
+        if ((numero[i] == 'I' && numero[i + 1] == 'I') && (numero[i] == 'I' && numero[i + 2] == 'I') && (numero[i] == 'I' && numero[i + 3] == 'I')) {
+            errores++;
+        }
+        if ((numero[i] == 'X' && numero[i + 1] == 'X') && (numero[i] == 'X' && numero[i + 2] == 'X') && (numero[i] == 'X' && numero[i + 3] == 'X')) {
+            errores++;
+        }
+        if ((numero[i] == 'C' && numero[i + 1] == 'C') && (numero[i] == 'C' && numero[i + 2] == 'C') && (numero[i] == 'C' && numero[i + 3] == 'C')) {
+            errores++;
+        }
+        if ((numero[i] == 'M' && numero[i + 1] == 'M') && (numero[i] == 'M' && numero[i + 2] == 'M') && (numero[i] == 'M' && numero[i + 3] == 'M')) {
+            errores++;
+        }
+        //No se permite la repeticion consecutiva de V, L y D
+        if ((numero[i] == 'V' && numero[i + 1] == 'V')) {
+            errores++;
+        }
+        if ((numero[i] == 'L' && numero[i + 1] == 'L')) {
+            errores++;
+        }
+        if ((numero[i] == 'D' && numero[i + 1] == 'D')) {
+            errores++;
+        }
     }
     if (errores != 0) {
         return false;
@@ -55,6 +83,45 @@ bool numeroEsCorrecto(char numero[]) {
     }
 }
 
+int calcularNumeroArabigo(char numero[]) {
+    int cantidad = 0;
+    for (int i = 0; numero[i] != '\0'; i++) {
+        switch (numero[i]) {
+        case 'I':
+            cantidad += 1;
+            break;
+        case 'V':
+            cantidad += 5;
+            break;
+        case 'X':
+            cantidad += 10;
+            break;
+        case 'L':
+            cantidad += 50;
+            break;
+        case 'C':
+            cantidad += 100;
+            break;
+        case 'D':
+            cantidad += 500;
+            break;
+        case 'M':
+            cantidad += 1000;
+            break;
+        }
+        if ((numero[i] == 'I' && numero[i + 1] == 'V') || (numero[i] == 'I' && numero[i + 1] == 'X')) {
+            cantidad -= 2;
+        }
+        if ((numero[i] == 'X' && numero[i + 1] == 'C') || (numero[i] == 'X' && numero[i + 1] == 'L')) {
+            cantidad -= 20;
+        }
+        if ((numero[i] == 'C' && numero[i + 1] == 'M') || (numero[i] == 'C' && numero[i + 1] == 'D')) {
+            cantidad -= 200;
+        }
+    }
+    return cantidad;
+}
+
 int main() {
     char numeroRomano[20];
     cout << "Hello World!\n";
@@ -62,7 +129,10 @@ int main() {
     cin >> numeroRomano;
     cout << numeroRomano << endl;
     if (numeroEsCorrecto(numeroRomano)) {
-        cout << "El Numero es correcto\n";
+        cout << calcularNumeroArabigo(numeroRomano) << endl;
+    }
+    else {
+        cout << "Vuelva a intentarlo\n";
     }
     return 0;
 }
